d9529ed5dfc9ec5683ff341bfc74edc18ca43939
evgenii <vovk.evgenii.199811@gmail.com>
2024-11-12 11:29:38 +0300

    ahange README

--- main.py
+++
@@ -1,8 +0,0 @@
-a1 = input()
-a2 = int(input())
-i = ["", "a", "b", "c", "d", "e", "f", "g", "h"]
-if a1 in i:
-    if i.index(a1) % 2 == 0 and a2 % 2 == 0:
-        print("YES")
-    else:
-        print("n")
