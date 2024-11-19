57ae62a129e22a3e61ecc91cefd1a743dbb77f4d
evgenii <vovk.evgenii.199811@gmail.com>
2024-11-19 16:47:54 +0300

    change

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
-phone_book = {"Erevan": 89996932314, "Igor": 89993323123, "Petr": 89992334411}
-for name in phone_book.keys():
-    print(name)
+str_1 = input()
+str_2 = input()
 
-for phone in phone_book.values():
-    print(phone)
 
-for key, value in phone_book.items():
-    print(key, "-", value)
+def get_tuple(string_1: str, string_2: str) -> tuple[list[str], list[str]]:
+    list_1 = string_1.split()
+    list_2 = string_2.split()
+    return list_1, list_2
 
-count_add_friends = int(input("Введите кол-во друзей: "))
-for _ in range(count_add_friends):
-    name_friends = input("Введите имя друга: ")
-    number_phone = int(input("Введите номер телефона: "))
-    phone_book[name_friends] = number_phone
-print(phone_book)
 
-name = input("Введите имя для кого хотите изменить номер ")
-phone = input("Введите номер ")
-phone_book[name] = phone
-print(phone_book)
+def get_dict(func):
+    def wrapper(*args) -> dict[str, str]:
+        res = func(*args)
+        dict_str = dict()
+        for key, value in zip(res[0], res[1]):
+            dict_str[key] = value
+        return dict_str
 
-name_frie = input("Введите имя для поиска и удаления: ")
-if name_frie in phone_book.keys():
-    del phone_book[name_frie]
-    print("Удалён")
-else:
-    print("Пользователь не найден")
-    pho = input("Введите номер телефона ненайденного пользователя ")
-    phone_book[name_frie] = pho
-print(phone_book)
+    return wrapper
+
+
+get_tuple = get_dict(get_tuple)
+d = get_tuple(str_1, str_2)
+print(*sorted(d.items()))
