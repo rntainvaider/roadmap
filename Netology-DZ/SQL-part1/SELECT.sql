547900b7295999f13e46755ba7ad70aaf8e2cfa8
evgenii <vovk.evgenii.199811@gmail.com>
2024-11-19 11:43:17 +0300

    change  Netology-DZ/SQL-part1

--- Netology-DZ/SQL-part1/SELECT.sql
+++ Netology-DZ/SQL-part1/SELECT.sql
@@ -20,22 +20,20 @@ WHERE position(' ' IN name) = 0;
 -- Название треков, которые содержат слово «моя» или «my».
 SELECT name
 FROM tracks
-WHERE name LIKE '%моя%'
-OR name LIKE '%my%';
+WHERE name = 'моя'
+OR name = 'my';
 
 -- Количество исполнителей в каждом жанре.
 SELECT genre.title, COUNT(artist_genre.genre_id) as Количество_исполнителей
 FROM genre
-JOIN artist_genre
-ON genre.id = artist_genre.genre_id
+JOIN artist_genre ON genre.id = artist_genre.genre_id
 GROUP BY genre.title;
 
 -- Количество треков, вошедших в альбомы 2019–2020 годов.
-SELECT COUNT(collections.age) as Кол_во_треков
-FROM tracks_collections
-JOIN collections
-ON tracks_collections.collection_id = collections.id
-WHERE collections.age BETWEEN 2019 AND 2020;
+SELECT COUNT(tracks.id)
+FROM tracks
+JOIN album ON album.id = tracks.album_id
+WHERE album.year BETWEEN 2018 AND 2020;
 
 -- Средняя продолжительность треков по каждому альбому.
 SELECT album.title, ROUND(AVG(tracks.duration), 2) as Средняя_продолжительность
@@ -48,30 +46,23 @@ GROUP BY album.title;
 SELECT artist.name
 FROM artist
 WHERE artist.id NOT IN (
-    SELECT album.id
+    SELECT artist_album.artist_id
     FROM album
-    JOIN artist_album ON artist_album.artist_id = album.id
+    JOIN artist_album ON artist_album.album_id = album.id
     WHERE album.year = 2017
 );
 
--- SELECT artist.name as "Все_исполнители"
--- FROM artist_album
--- JOIN artist
--- ON artist.id = artist_album.artist_id
--- JOIN album
--- ON album.id = artist_album.album_id
--- WHERE album.year != 2017
--- GROUP BY artist.name;
-
 -- Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).SELECT DISTINCT album.title
-FROM album
-JOIN tracks ON tracks.id = album_id
-JOIN artist ON artist.id = album.id
+SELECT DISTINCT collections.name
+FROM collections
+JOIN tracks_collections ON collections.id = tracks_collections.collection_id
+JOIN tracks ON tracks.id = tracks_collections.tracks_id
+JOIN album ON album.id = tracks.album_id
+JOIN artist_album ON artist_album.album_id = album.id
+JOIN artist ON artist_album.artist_id = artist.id
 WHERE artist.name = 'Zivert';
 
 -- Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
 -- Наименования треков, которые не входят в сборники.
 -- Исполнитель или исполнители, написавшие самый короткий по продолжительности трек, — теоретически таких треков может быть несколько.
-
-
 -- Названия альбомов, содержащих наименьшее количество треков.
