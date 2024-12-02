bda0fb7b14ca0feb6a630a8ac32a837116a9d9f1
evgenii <vovk.evgenii.199811@gmail.com>
2024-12-02 15:24:34 +0300

    change

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-def sort_users_by_activity(dicty):
-    return sorted(
-        user_activity.keys(), key=lambda user: user_activity[user], reverse=True
-    )
+weekdays = [
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+]
 
+days = ((day, weekdays[(day - 1) % 7]) for day in range(1, 366))
 
-user_activity = {"user1": 10, "user2": 5, "user3": 20, "user4": 15, "user5": 10}
-print(sort_users_by_activity(user_activity))
-# ['user3', 'user4', 'user1', 'user5', 'user2']
+for day in list(days)[:77]:
+    print(day)
