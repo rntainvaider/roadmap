ea196e794a90cd4c8b56b73ecdb06801f255fb62
evgenii <vovk.evgenii.199811@gmail.com>
2024-12-17 16:46:29 +0300

    change

--- DJANGO book 1/mysite/blog/models.py
+++ DJANGO book 1/mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -39,3 +40,6 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.id])
